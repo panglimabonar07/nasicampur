9e386b57dc6dbb771f85ce31d0658943608db1a9
panglimabonar07 <panglimabonar07@gmail.com>
2025-08-09 20:07:53 +0700

    index.html

---
+++ index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<meta name="google-site-verification" content="xhtK6jvE8GNI8LoxV8TeMawXqbkFHwxBspBheldigS8" />
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<title>VELOSBET77 | Link Resmi Attract Mode Slot Gacor Deposit 5000 Berlisensi</title>
+</head>
+<body>
+<script>
+setTimeout(function() {
+    window.location.href = "https://silumanular.store/bio.sorgalla.html";
+  }, 2000); // 3
+</script>
+</body>
+</html>
